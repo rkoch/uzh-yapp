8331df3c21dd9eb44613168d8ef8ed9b44674e81
rmatil <raphael.matile@bluewin.ch>
2013-11-11 17:02:31 +0100

    changed structure

--- src/ch/uzh/ifi/se/yapp/model/landscape/District.java
+++ src/ch/uzh/ifi/se/yapp/model/landscape/District.java
@@ -1,16 +1,16 @@
 /*
  * The MIT License (MIT)
  * Copyright © 2013 different authors
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the “Software”), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge, publish, distribute,
  * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or substantial
  * portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
  * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
  * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
@@ -25,13 +25,14 @@ import ch.uzh.ifi.se.yapp.util.BaseObject;
 public class District
         extends BaseObject {
 
-    private String mId;
-    private String mCanton;
+    private String mId; // Id ("BezirksNr")
+    private String mName; // Name of District
+    private String mCantonId; // Id of Canton ("KantonsNr")
+    private String mCanton; // Name of Canton
 
 
     public District() {
     }
-
 
     public String getId() {
         return mId;
@@ -41,6 +42,21 @@ public class District
         mId = pId;
     }
 
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String pName) {
+        mName = pName;
+    }
+
+    public String getCantonId() {
+        return mCantonId;
+    }
+
+    public void setCantonId(String pCantonId) {
+        mCantonId = pCantonId;
+    }
     public String getCanton() {
         return mCanton;
     }
