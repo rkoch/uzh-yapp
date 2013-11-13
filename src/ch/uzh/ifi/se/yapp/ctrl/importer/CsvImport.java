39d36cf74fa8edfb1fa2bc0acc2ae6e6d73b8837
Roman Brenn <romanbrenn@gmail.com>
2013-11-13 14:33:22 +0100

    Adds method for date import and changed DistrictResult attributes and methods

--- src/ch/uzh/ifi/se/yapp/ctrl/importer/CsvImport.java
+++ src/ch/uzh/ifi/se/yapp/ctrl/importer/CsvImport.java
@@ -22,40 +22,123 @@ package ch.uzh.ifi.se.yapp.ctrl.importer;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
-import ch.uzh.ifi.se.yapp.model.landscape.District;
+import org.joda.time.LocalDate;
+
+import ch.uzh.ifi.se.yapp.backend.accif.BackendAccessorFactory;
+import ch.uzh.ifi.se.yapp.backend.accif.IElectionDataAdapter;
 import ch.uzh.ifi.se.yapp.model.landscape.DistrictResult;
 import ch.uzh.ifi.se.yapp.model.landscape.Election;
 
 
 public class CsvImport {
 
-    public void importElection(File pFile) {
-        Election pElection = new Election();
+    /**
+     * <b>importElection</b> <br>
+     * Description: import a csv file and instance an election and save it on the server
+     *
+     * @param pFile
+     * @throws IOException
+     */
+    public void importElection(File pFile)
+            throws IOException {
+        try {
+            Election pElection = new Election();
 
-        BufferedReader br = new BufferedReader(new FileReader(pFile));
-        String line = null;
-        int count = 0;
+            BufferedReader br = new BufferedReader(new FileReader(pFile));
+            String line = null;
+            int count = 0;
 
-        while((line = br.readLine()) != null) {
-            DistrictResult pDResult = new DistrictResult();
-            District district = new District();
+            //reads .csv until all lines are finished
+            while ((line = br.readLine()) != null) {
+                DistrictResult pDResult = new DistrictResult();
 
-            String[] cells = line.split(";");
+                String[] cells = line.split(";");
 
-            if(count == 0) {
-                pElection.setId(cells[2]);
-            } else if(count == 1) {
-                pElection.setTitle(cells[0]);
-            } else {
-                district.setId(cells[0]);
-                district.setName(cells[1]);
+                //writes data in DistrictResult
+                if (count == 0) {
+                    pElection.setDate(getDate(cells[0]));
+                    pElection.setId(cells[2]);
+                } else if (count == 1) {
+                    pElection.setTitle(cells[0]);
+                } else {
+                    pDResult.getDistrict().setId(cells[0]);
+                    pDResult.getDistrict().setName(cells[1]);
+                    pDResult.setTotalEligibleCount(Integer.parseInt(cells[2]));
+                    pDResult.setDeliveredVoteCount(Integer.parseInt(cells[3]));
+                    pDResult.setValidVoteCount(Integer.parseInt(cells[5]));
+                    pDResult.setYesVoteCount(Integer.parseInt(cells[6]));
+                    pDResult.setNoVoteCount(Integer.parseInt(cells[7]));
+                }
+                count++;
 
+                //adds DistrictResult to the Election
+                pElection.getResults().add(pDResult);
             }
 
-            count++;
+            br.close();
+
+            // saves Election on server (Google App Engine)
+            IElectionDataAdapter adpt = BackendAccessorFactory.getElectionDataAdapter();
+            adpt.insertElection(pElection);
+        } catch (IOException e) {
+            System.out.println("ERRO: IOException.");
+        }
+    }
+
+    /** <b>getDate</b>
+     * <br>Description: converts a String to a yoda LocalDate
+     * @param pDate
+     * @return LocalDate
+     */
+    public LocalDate getDate(String pDate) {
+        String[] fragments = pDate.replace(".", "").split(" ");
+
+        int day = Integer.parseInt(fragments[3]);
+        int month = 0;
+        int year = Integer.parseInt(fragments[5]);
+
+        switch(fragments[4]) {
+            case "Januar":
+                month = 1;
+                break;
+            case "Februar":
+                month = 2;
+                break;
+            case "März":
+                month = 3;
+                break;
+            case "April":
+                month = 4;
+                break;
+            case "Mai":
+                month = 5;
+            case "Juni":
+                month = 6;
+                break;
+            case "Juli":
+                month = 7;
+                break;
+            case "August":
+                month = 8;
+                break;
+            case "September":
+                month = 9;
+                break;
+            case "Oktober":
+                month = 10;
+                break;
+            case "November":
+                month = 11;
+                break;
+            case "Dezember":
+                month = 12;
+                break;
         }
 
-        br.close();
+        LocalDate date = new LocalDate(year,month,day);
+
+        return date;
     }
 }
