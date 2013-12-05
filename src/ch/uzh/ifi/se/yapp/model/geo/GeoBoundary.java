9df44e63a72bebac6da01f1988d3a2d4210ada85
Raphael Matile <raphael.matile@bluewin.ch>
2013-12-05 20:48:55 +0100

    Changes in copy constructor: mDate is always replaced by the value of pOrig.getDate

--- src/ch/uzh/ifi/se/yapp/model/geo/GeoBoundary.java
+++ src/ch/uzh/ifi/se/yapp/model/geo/GeoBoundary.java
@@ -49,9 +49,7 @@ public class GeoBoundary
 
     public GeoBoundary(GeoBoundary pOrig) {
         mId = pOrig.getId();
-        if (mDate != null) {
-            mDate = new LocalDate(pOrig.mDate.toString());
-        }
+        mDate = new LocalDate(pOrig.mDate.toString());
         mPolygons = new HashSet<>(pOrig.mPolygons);
     }
 
